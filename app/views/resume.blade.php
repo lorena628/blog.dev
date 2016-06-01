1957401ce4164c2c48511117ac583b2f06d171e6
lo v <lov@los-MacBook-Pro.local>
2016-06-01 14:41:17 -0500

    recreated the resume to be included in this repo

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -1,5 +1,42 @@
 @extends('layouts.master')
 
-@section('content')
-    <h1>This is my resume!</h1>
+@section('topscript')
+<title>Lorena's Super Awesome Resume</title>
+@stop
+
+
+
+
+@section('content')    
+    <h1>Lorena Villanueva</h1>
+    <p>5510 Ivanhoe St. <br>San Antonio, Texas 78228<br>(210)897-1591<br><a href="mailto:#">lorena.villanueva@yahoo.com</a></p>       
+    
+        <div id="header">
+            <h3 class="heading">Introduction</h3>
+        
+            <p>My name is Lorena Villanueva and I have acquired a very well rounded set of skills specific to customer-service, education, and accounting through both my current and previous places of employment as well as volunteer work. I’m an efficient, independent worker and am eager to explore new avenues.</p>
+
+            <div id="content">
+                
+                <h3 class="heading">Education</h3>
+                    <p><strong>San Antonio College  GPA: 3.0</strong> <br>20 Hours 2010-2011</p>
+                    <p><strong>University Of Texas San Antonio GPA: 3.2</strong><br>21 Hours 2009-2010
+                    </p>
+
+                <h3 class="heading">Employment</h3>
+                   <p> <strong>Via Metropolitan Transit 08/14-Current</strong><br>Reservation agent assisting clients in making their daily essntial trips.</p>
+                   <p><strong>NISD School Tutor 09/11-06/14</strong><br> Tutored middle school-aged children in math and science</p>
+                   <p><strong>Santana Health Care Provider 04/11-07/14</strong><br>Assisted client with daily activities such as cleaning and cooking</p>
+                   <p><strong>Mi Tierra Café 2010 Summer</strong><br>Waitress and cashier</p>
+
+
+                <h3 class="heading">Activities</h3>
+                    <p><strong>Accounting Volunteer 2011-2014</strong><br> Volunteer in an accounting department
+                   I have learned various skills and acquired experience in itemizing, spreadsheets, purchasing, balancing of checking disbursements, billing etc.</p>
+
+                </div>   
+        
+            </div>
+        </div>            
+    </div>
 @stop
