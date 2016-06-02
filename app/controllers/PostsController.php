64af596ecff2328dc30304afe72a0098a123683b
lo v <lov@los-MacBook-Pro.local>
2016-06-02 13:23:03 -0500

    edited the create and show functions to show the create view along with a "redirect" of old input

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -20,7 +20,7 @@ class PostsController extends \BaseController {
 	 */
 	public function create()
 	{
-		return 'all posts created';
+		return View::make("/posts/create");
 	}
 
 
@@ -31,7 +31,7 @@ class PostsController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+		return Redirect::back()->withInput();
 	}
 
 
