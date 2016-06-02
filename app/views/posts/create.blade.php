cffb81bfc25c8596d7d8287e33b5645b65ac3353
lo v <lov@los-MacBook-Pro.local>
2016-06-02 13:41:50 -0500

    added the "old input" for text area

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.master')
 
-@section('content')
+@section('topscript')
+<link rel="stylesheet" type="text/css" href="/css/createpost.css">
+<link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+@stop
 
+@section('content')
 <h1>Create a blog post</h1>
     <form method="POST" action="{{{ action('PostsController@store') }}}">
             
@@ -10,7 +14,7 @@
             <p><input class="forms" type="text" name="description" placeholder="Description: " value="{{{ Input::old('description') }}}"></p>
 
             
-                <textarea placeholder= "Content:" class="form" name= "content"id="textarea" rows="7" value="{{{ Input::old('content') }}}"></textarea>
+                <textarea placeholder= "Content:" class="form" name= "content"id="textarea" rows="7">{{{ Input::old('content') }}}</textarea>
             
 
             <p><input class="forms" type="text" name="category" placeholder="Category: " value="{{{ Input::old('category') }}}"></p>
