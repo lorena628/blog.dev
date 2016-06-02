73df0f4d3a75639805667beabfaa228e06cf24e7
lo v <lov@los-MacBook-Pro.local>
2016-06-02 13:50:14 -0500

    added an icon

--- app/views/posts/create.blade.php
+++ app/views/posts/create.blade.php
@@ -3,10 +3,11 @@
 @section('topscript')
 <link rel="stylesheet" type="text/css" href="/css/createpost.css">
 <link href='https://fonts.googleapis.com/css?family=Josefin+Sans' rel='stylesheet' type='text/css'>
+<script src="https://use.fontawesome.com/f5fdf2e9f7.js"></script>
 @stop
 
 @section('content')
-<h1>Create a blog post</h1>
+<h1>Create a blog post <i class="fa fa-pencil-square-o " aria-hidden="true"></i></h1>
     <form method="POST" action="{{{ action('PostsController@store') }}}">
             
             <p><input class="forms" type="text" name="title" placeholder="Title: " value="{{{ Input::old('title') }}}"></p>
