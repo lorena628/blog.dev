20a9d79606001f0267eb92cbe0b9b8c950085f41
lo v <lov@los-MacBook-Pro.local>
2016-06-03 09:54:03 -0500

    added to catch errors

--- app/models/Post.php
+++ app/models/Post.php
@@ -3,4 +3,13 @@
 class Post extends Eloquent
 {
     protected $table = "posts";
+
+
+    public static $rules = array(
+        'title'      => 'required|max:100',
+        'content'       => 'required|max:100000',
+        'description'  => 'required|max:100',
+        'category' => 'required|max:100'
+    );
+
 }
