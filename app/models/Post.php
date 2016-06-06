0ed571176f3cc3a8ccb06efd0d951d13542d723c
lo v <lov@los-MacBook-Pro.local>
2016-06-06 15:39:15 -0500

    user function to show that posts belong to that particular user

--- app/models/Post.php
+++ app/models/Post.php
@@ -7,9 +7,13 @@ class Post extends BaseModel
 
     public static $rules = array(
         'title'      => 'required|max:100',
-        'content'       => 'required|max:100000',
+        'content'       => 'required|max:10000000',
         'description'  => 'required|max:100',
         'category' => 'required|max:100'
     );
 
+    public function user() //each post belongs to a single user
+    {
+        return $this->belongsTo('User');
+    }
 }
