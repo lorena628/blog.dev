9e8fce92146332dae605f42c57aa1f8f9ffa7d75
lo v <lov@los-MacBook-Pro.local>
2016-06-06 13:03:28 -0500

    minimal styling

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.master')
 
+@section('upperbody')
+<style>
+
+        body {
+            /*color: white;*/
+            text-align: center;
+            background-color: black;
+        }
+        h1 {
+            font-size: 15em;
+            color: white;
+            text-align: center;
+        }
+        h2 {
+            font-size: 3em;
+            color: white;
+            text-align: center;
+            margin-bottom: 8em;
+        }
+ 
+</style>
+@stop
+
+
 @section('content')
-<h1>this is an error page 404 <h1>
+<h1>404<h1>
+<h2>Not Found.</h2>
+
+
+<a href="/posts">Take me home</a>
 @stop
 
