869223dc24cdea57b9a21f7f555f1d8f384ce7e2
lo v <lov@los-MacBook-Pro.local>
2016-06-07 21:17:38 -0500

    did minor changes

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -4,18 +4,32 @@
 <link rel="stylesheet" type="text/css" href="/css/login.css">
 @stop
 
-@section('content')
-<h1>Please login</h1>
+@section('abovecontainer')
+<div class="container">
+    <div id="filler">
+        <h1>Please login</h1>
 
-{{ Form::open(array('action' =>('HomeController@doLogin'), 'method' => 'POST')) }}
+            {{ Form::open(array('action' =>('HomeController@doLogin'), 'method' => 'POST')) }}
 
-    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email')) }}
-    {{ Form::password('password',  null,  array('class' =>'form-control', 'placeholder'=> 'Password')) }}
+            <div class="row">
+                <div class="col-md-4  col-md-offset-4">
+                    {{ Form::label('email', 'Email', array('class' => 'sr-only')) }}
+                    {{ Form::text('email',  null,  array('class' =>'form-control', 'placeholder'=> 'Email','id'=>'email-field')) }}
+                </div>
+            </div>
 
+            <div class="row">
+                <div class="col-md-4  col-md-offset-4">
+                    {{ Form::label('password', 'Password', array('class' => 'sr-only')) }}
+                    {{ Form::password('password', array('class'=>'form-control', 'placeholder'=>'Password', 'id'=>'pass-field')) }}
+                </div>
+            </div>
 
-    <p><input class="btn btn-success" id="btn_sub"  type="submit" value="Login"></p>
-
-
-{{ Form::close() }}
+                <div id="button">
+                    <input class="btn btn-success" id="btn_sub"  type="submit" value="Login">
+                </div>
+            {{ Form::close() }}
+    </div>
+</div>
 @stop
 
