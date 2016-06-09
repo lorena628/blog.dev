64f84ed37d44dddc46bdd73a9bdff81bd8ee6537
lo v <lov@los-MacBook-Pro.local>
2016-06-09 10:26:07 -0500

    changed path to css file

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -7,7 +7,7 @@
    <!-- font awesomes -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
 
-    <link rel="stylesheet" href="/css/portfolio.css">
+    <link rel="stylesheet" href="/../css/portfolio.css">
 @stop
 
 
