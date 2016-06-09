1e3ebb1aef45e7dddb747e981a3844aff0dedf20
lo v <lov@los-MacBook-Pro.local>
2016-06-09 11:09:46 -0500

    changed navbar

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -20,12 +20,12 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="">Blog.dev</a>
+      <a class="navbar-brand" href="/">Home</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Posts<span class="sr-only">(current)</span></a></li>
+        <li class="active"><a href="#">All Posts<span class="sr-only">(current)</span></a></li>
         <li><a href="/posts/create">Create a post</a></li>
        <!--  <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Options <span class="caret"></span></a>
