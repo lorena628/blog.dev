9e6871ba9967eef99a21bf596790c367fce0acf6
lo v <lov@los-MacBook-Pro.local>
2016-06-10 11:53:41 -0500

    did things

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -22,7 +22,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/">Home</a>
+      <a class="navbar-brand" href="/"><img src="/img/LM.png"></a>
     </div>
 
 
