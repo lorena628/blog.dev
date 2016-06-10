c5345193264bc4de1315bfe21fecf5d4ecbfcc57
lo v <lov@los-MacBook-Pro.local>
2016-06-09 22:07:17 -0500

    testing to see responsiveness in phone

--- app/views/portfolio.blade.php
+++ app/views/portfolio.blade.php
@@ -5,7 +5,7 @@
 
     <title>lorenamarie</title>
 
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- google fonts -->
    <link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
    <!-- font awesomes -->
