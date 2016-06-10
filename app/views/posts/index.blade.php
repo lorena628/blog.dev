e7aee0e290bf57d4120693830794e10bda8f540e
lo v <lov@los-MacBook-Pro.local>
2016-06-10 12:04:11 -0500

    changed pics

--- app/views/posts/index.blade.php
+++ app/views/posts/index.blade.php
@@ -22,7 +22,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/"><img src="/img/LM2.png"></a>
+      <a class="navbar-brand" href="/"><img src="/img/LM.png" id="pic"></a>
     </div>
 
 
