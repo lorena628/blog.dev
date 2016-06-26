7e3dd5ad9567392c2309af3ea4c179f34a96eca5
lo v <lov@los-MacBook-Pro.local>
2016-06-25 23:19:57 -0500

    silly 404

--- app/views/errors/missing.blade.php
+++ app/views/errors/missing.blade.php
@@ -18,6 +18,9 @@
             color: white;
             text-align: center;
         }
+        h3{
+            color: gray;
+        }
  
 </style>
 @stop
@@ -27,7 +30,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <h1>404<h1>
 <h2>Not Found.</h2>
-<h3>enjoy this picture of of me doing child labor</h3>
+<h3>...but enjoy this picture of me doing child labor</h3>
 <img src="/img/childlabor.jpeg">
 
 <p><a href="/">Take me home</a></p>
